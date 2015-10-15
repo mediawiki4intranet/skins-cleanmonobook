238d0919656ef86e87e953ad9a0baca64a4f27db
Vitaliy Filippov <vitalif@yourcmc.ru>
2015-10-15 15:15:17 +0300

    Update skin for MediaWiki 1.25+

--- cleanmonobook.php
+++ cleanmonobook.php
@@ -41,30 +41,18 @@ class SkinCleanmonobook extends SkinTemplate {
 	}
 
 	function setupSkinUserCss( OutputPage $out ) {
-		global $wgHandheldStyle;
-
 		parent::setupSkinUserCss( $out );
-
-		// Append to the default screen common & print styles...
-		$out->addStyle( 'cleanmonobook/main.css', 'screen' );
-		$out->addStyle( 'monobook/rtl.css', 'screen', '', 'rtl' );
+		$out->addModuleStyles( array(
+			'mediawiki.skinning.interface',
+			'mediawiki.skinning.content.externallinks',
+			'skins.cleanmonobook'
+		) );
 	}
 }
 
-/**
- * @todo document
- * @ingroup Skins
- */
-class CleanmonobookTemplate extends QuickTemplate {
+class CleanmonobookTemplate extends BaseTemplate {
 	var $skin;
-	/**
-	 * Template filter callback for MonoBook skin.
-	 * Takes an associative array of data set from a SkinTemplate-based
-	 * class, and a wrapper for MediaWiki's localization database, and
-	 * outputs a formatted page.
-	 *
-	 * @access private
-	 */
+
 	function execute() {
 		global $wgRequest;
 		$this->skin = $skin = $this->data['skin'];
@@ -72,80 +60,83 @@ class CleanmonobookTemplate extends QuickTemplate {
 
 		// Suppress warnings to prevent notices about missing indexes in $this->data
 		wfSuppressWarnings();
-
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="<?php $this->text('xhtmldefaultnamespace') ?>" <?php
-	foreach($this->data['xhtmlnamespaces'] as $tag => $ns) {
-		?>xmlns:<?php echo "{$tag}=\"{$ns}\" ";
-	} ?>xml:lang="<?php $this->text('lang') ?>" lang="<?php $this->text('lang') ?>" dir="<?php $this->text('dir') ?>">
-	<head>
-		<meta http-equiv="Content-Type" content="<?php $this->text('mimetype') ?>; charset=<?php $this->text('charset') ?>" />
-		<?php $this->html('headlinks') ?>
-		<title><?php $this->text('pagetitle') ?></title>
-		<?php $this->html('csslinks') ?>
-
-		<!--[if lt IE 7]><script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('stylepath') ?>/common/IEFixes.js?<?php echo $GLOBALS['wgStyleVersion'] ?>"></script>
-		<meta http-equiv="imagetoolbar" content="no" /><![endif]-->
-
-		<?php print Skin::makeGlobalVariablesScript( $this->data ); ?>
-
-		<script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('stylepath' ) ?>/common/wikibits.js?<?php echo $GLOBALS['wgStyleVersion'] ?>"><!-- wikibits js --></script>
-		<!-- Head Scripts -->
-<?php $this->html('headscripts') ?>
-<?php	if($this->data['jsvarurl']) { ?>
-		<script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('jsvarurl') ?>"><!-- site js --></script>
-<?php	} ?>
-<?php	if($this->data['pagecss']) { ?>
-		<style type="text/css"><?php $this->html('pagecss') ?></style>
-<?php	}
-		if($this->data['usercss']) { ?>
-		<style type="text/css"><?php $this->html('usercss') ?></style>
-<?php	}
-		if($this->data['userjs']) { ?>
-		<script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('userjs' ) ?>"></script>
-<?php	}
-		if($this->data['userjsprev']) { ?>
-		<script type="<?php $this->text('jsmimetype') ?>"><?php $this->html('userjsprev') ?></script>
-<?php	}
-		if($this->data['trackbackhtml']) print $this->data['trackbackhtml']; ?>
-	</head>
-<body<?php if(!empty($this->data['body_ondblclick'])) { ?> ondblclick="<?php $this->text('body_ondblclick') ?>"<?php } ?>
-<?php if(!empty($this->data['body_onload'])) { ?> onload="<?php $this->text('body_onload') ?>"<?php } ?>
- class="mediawiki <?php $this->text('dir') ?> <?php $this->text('pageclass') ?> <?php $this->text('skinnameclass') ?>">
-	<div id="globalWrapper">
+		$this->html( 'headelement' );
+		?>
+		<div id="globalWrapper">
 		<div id="column-content">
-	<div id="content">
-		<a name="top" id="top"></a>
-		<?php if($this->data['sitenotice']) { ?><div id="siteNotice"><?php $this->html('sitenotice') ?></div><?php } ?>
-		<h1 id="firstHeading" class="firstHeading"><?php $this->data['displaytitle']!=""?$this->html('title'):$this->text('title') ?></h1>
-		<div id="bodyContent">
-			<h3 id="siteSub"><?php $this->msg('tagline') ?></h3>
-			<div id="contentSub"><?php $this->html('subtitle') ?></div>
-			<?php if($this->data['undelete']) { ?><div id="contentSub2"><?php     $this->html('undelete') ?></div><?php } ?>
-			<?php if($this->data['newtalk'] ) { ?><div class="usermessage"><?php $this->html('newtalk')  ?></div><?php } ?>
-			<?php if($this->data['showjumplinks']) { ?><div id="jump-to-nav"><?php $this->msg('jumpto') ?> <a href="#column-one"><?php $this->msg('jumptonavigation') ?></a>, <a href="#searchInput"><?php $this->msg('jumptosearch') ?></a></div><?php } ?>
-			<!-- start content -->
-			<?php $this->data['bodytext'] = preg_replace_callback( '#<div class="printfooter">(.*?)</div#is', function( $m ) { return preg_replace( '#&amp;oldid=\d+#', '', $m[0] ); }, $this->data['bodytext'] ); ?>
-			<?php $this->html('bodytext') ?>
-			<?php if($this->data['catlinks']) { $this->html('catlinks'); } ?>
-			<!-- end content -->
-			<?php if($this->data['dataAfterContent']) { $this->html ('dataAfterContent'); } ?>
-			<div class="visualClear"></div>
-		</div>
-	</div>
-		</div>
-	</div>
-<?php $this->html('bottomscripts'); /* JS call to runBodyOnloadHook */ ?>
-<?php $this->html('reporttime') ?>
-<?php if ( $this->data['debug'] ): ?>
-<!-- Debug output:
-<?php $this->text( 'debug' ); ?>
+			<div id="content" class="mw-body" role="main">
+				<a id="top"></a>
+				<?php
+				if ( $this->data['sitenotice'] ) {
+					?>
+					<div id="siteNotice"><?php
+					$this->html( 'sitenotice' )
+					?></div><?php
+				}
+				?>
 
--->
-<?php endif; ?>
-</body></html>
-<?php
-	wfRestoreWarnings();
+				<?php
+				echo $this->getIndicators();
+				// Loose comparison with '!=' is intentional, to catch null and false too, but not '0'
+				if ( $this->data['title'] != '' ) {
+				?>
+				<h1 id="firstHeading" class="firstHeading" lang="<?php
+				$this->data['pageLanguage'] =
+					$this->getSkin()->getTitle()->getPageViewLanguage()->getHtmlCode();
+				$this->text( 'pageLanguage' );
+				?>"><?php $this->html( 'title' ) ?></h1>
+				<?php } ?>
+
+				<div id="bodyContent" class="mw-body-content">
+					<div id="siteSub"><?php $this->msg( 'tagline' ) ?></div>
+					<div id="contentSub"<?php
+					$this->html( 'userlangattributes' ) ?>><?php $this->html( 'subtitle' )
+						?></div>
+					<?php if ( $this->data['undelete'] ) { ?>
+						<div id="contentSub2"><?php $this->html( 'undelete' ) ?></div>
+					<?php
+}
+					?><?php
+					if ( $this->data['newtalk'] ) {
+						?>
+						<div class="usermessage"><?php $this->html( 'newtalk' ) ?></div>
+					<?php
+					}
+					?>
+					<div id="jump-to-nav" class="mw-jump"><?php
+						$this->msg( 'jumpto' )
+						?> <a href="#column-one"><?php
+							$this->msg( 'jumptonavigation' )
+							?></a><?php
+						$this->msg( 'comma-separator' )
+						?><a href="#searchInput"><?php
+							$this->msg( 'jumptosearch' )
+							?></a></div>
+
+					<!-- start content -->
+					<?php $this->html( 'bodytext' ) ?>
+					<?php
+					if ( $this->data['catlinks'] ) {
+						$this->html( 'catlinks' );
+					}
+					?>
+					<!-- end content -->
+					<?php
+					if ( $this->data['dataAfterContent'] ) {
+						$this->html( 'dataAfterContent'
+						);
+					}
+					?>
+					<div class="visualClear"></div>
+				</div>
+			</div>
+		</div>
+		<?php
+		$this->printTrail();
+		echo Html::closeElement( 'body' );
+		echo Html::closeElement( 'html' );
+		echo "\n";
+		wfRestoreWarnings();
 	} // end of execute() method
 
 } // end of class
